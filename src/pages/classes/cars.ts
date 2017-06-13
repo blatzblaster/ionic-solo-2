058665da8060762b559266f8b0d88e287d551753
Bill Sand <bildonia@gmail.com>
2017-06-12 21:36:52 -0500

    Add A and B Street classes

---
+++ src/pages/classes/cars.ts
@@ -0,0 +1,828 @@
+let cars = [{
+    year: [2017],
+    make: "Acura",
+    model: "NSX",
+    class: "SS"
+},
+{
+    years: [2015, 2016, 2017],
+    make: "Alfa Romeo",
+    model: "4C",
+    class: "SS"
+},
+{
+    years: [2012, 2013],
+    make: "Audi",
+    model: "TT RS",
+    class: "SS"
+},
+{
+    years: [2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015],
+    make: "Audi",
+    model: "R8 (V8)",
+    class: "SS"
+},
+{
+    years: [],
+    make: "BMW",
+    model: "Z8",
+    class: "SS"
+},
+{
+    years: [2017],
+    make: "Chevrolet",
+    model: "Corvette Grand Sport",
+    class: "SS"
+},
+{
+    years: [2015, 2016, 2017],
+    make: "Chevrolet",
+    model: "Corvette Z06 (C7)",
+    class: "SS"
+},
+{
+    years: [2009, 2010, 2011, 2012, 2013],
+    make: "Chevrolet",
+    model: "Corvette ZR1",
+    class: "SS"
+},
+{
+    years: [2016, 2017],
+    make: "Dodge",
+    model: "Viper ACR",
+    class: "SS"
+},
+{
+    years: [2013, 2014],
+    make: "Dodge",
+    model: "Viper/Viper GTS (non-ACR, nonTA)",
+    class: "SS"
+},
+{
+    years: [2015, 2016, 2017],
+    make: "Dodge",
+    model: "Viper SRT (non-ACR, non-TA 2.0)",
+    class: "SS"
+},
+{
+    years: [],
+    make: "Dodge",
+    model: "Viper (Not Otherwise Classed)",
+    class: "SS"
+},
+{
+    years: [1999, 2000, 2001, 2002, 2003, 2004, 2005],
+    make: "Ferrari",
+    model: "360 Modena & Spider (non-Chal- lenge Stradale)",
+    class: "SS"
+},
+{
+    years: [1993, 1995, 2000],
+    make: "Ford",
+    model: "Mustang Cobra R",
+    class: "SS"
+},
+{
+    years: [2016, 2017],
+    make: "Ford",
+    model: "Mustang Shelby GT350R",
+    class: "SS"
+},
+{
+    years: [2005, 2006, 2007, 2008, 2009, 2010, 2011],
+    make: "Lotus",
+    model: "Elise (non-SC)",
+    class: "SS"
+},
+{
+    years: [2011, 2012, 2013, 2014, 2015],
+    make: "Lotus",
+    model: "Evora S",
+    class: "SS"
+},
+{
+    years: [2006],
+    make: "Lotus",
+    model: "Exige (non-supercharged)",
+    class: "SS"
+},
+{
+    years: [2012, 2013, 2014],
+    make: "McLaren",
+    model: "MP4-12C",
+    class: "SS"
+},
+{
+    years: [],
+    make: "Mercedes",
+    model: "AMG (Not Otherwise Classed)",
+    class: "SS"
+},
+{
+    years: [2009, 2010, 2011],
+    make: "Nissan",
+    model: "GT-R",
+    class: "SS"
+},
+{
+    years: [2012, 2013, 2014, 2015, 2016, 2017],
+    make: "Nissan",
+    model: "GT-R (excluding NISMO & Track Edition)",
+    class: "SS"
+},
+{
+    years: [2017],
+    make: "Porsche",
+    model: "718 Boxster S",
+    class: "SS"
+},
+{
+    years: [2017],
+    make: "Porsche",
+    model: "718 Cayman S",
+    class: "SS"
+},
+{
+    years: [2012, 2013, 2014, 2015, 2016, 2017],
+    make: "Porsche",
+    model: "911 (991, incl. GT3; non-turbo, non-GT3 RS)",
+    class: "SS"
+},
+{
+    years: [2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012],
+    make: "Porsche",
+    model: "911 (997 chassis)",
+    class: "SS"
+},
+{
+    years: [],
+    make: "Porsche",
+    model: "911 GT3 (996 & 997, non-RS)",
+    class: "SS"
+},
+{
+    years: [2001, 2002, 2003, 2004, 2005],
+    make: "Porsche",
+    model: "911 Turbo & Turbo S (996 chassis)",
+    class: "SS"
+},
+{
+    years: [1989, 1990, 1991, 1992, 1993, 1994],
+    make: "Porsche",
+    model: "911 Turbo (964, non-S, non-3.6S)",
+    class: "SS"
+},
+{
+    years: [1974, 1975, 1976, 1977, 1978, 1979, 1980, 1981, 1982, 1983, 1984, 1985, 1986, 1987, 1988, 1989],
+    make: "Porsche",
+    model: "911 Turbo (930)",
+    class: "SS"
+},
+{
+    years: [2015, 2016],
+    make: "Porsche",
+    model: "Boxster GTS",
+    class: "SS"
+},
+{
+    years: [2013, 2014, 2015, 2016],
+    make: "Porsche",
+    model: "Boxster S (981 chassis)",
+    class: "SS"
+},
+{
+    years: [2011, 2012, 2013],
+    make: "Porsche",
+    model: "Boxster Spyder",
+    class: "SS"
+},
+{
+    years: [2016],
+    make: "Porsche",
+    model: "Cayman GT4 (non-Clubsport)",
+    class: "SS"
+},
+{
+    years: [2015, 2016],
+    make: "Porsche",
+    model: "Cayman GTS",
+    class: "SS"
+},
+{
+    years: [2012],
+    make: "Porsche",
+    model: "Cayman R",
+    class: "SS"
+},
+{
+    years: [2013, 2014, 2015, 2016],
+    make: "Porsche",
+    model: "Cayman S (981 chassis)",
+    class: "SS"
+},
+{
+    years: [2008, 2009, 2010, 2011, 2012, 2013],
+    make: "Tesla Motors",
+    model: "Roadster (all)",
+    class: "SS"
+},
+{
+    years: [],
+    make: "Catch All",
+    model: "",
+    class: "SS"
+},
+{
+    years: [],
+    make: "Acura",
+    model: "NSX Alex Zanardi Signature Edition",
+    class: "AS"
+},
+{
+    years: [2016, 2017],
+    make: "Audi",
+    model: "TTS",
+    class: "AS"
+},
+{
+    years: [],
+    make: "BMW",
+    model: "M2 Performance Edition ZL9",
+    class: "AS"
+},
+{
+    years: [2015, 2016, 2017],
+    make: "BMW",
+    model: "M3 (F80)",
+    class: "AS"
+},
+{
+    years: [2015, 2016, 2017],
+    make: "BMW",
+    model: "M4 (F82)",
+    class: "AS"
+},
+{
+    years: [],
+    make: "Cadillac",
+    model: "XLR",
+    class: "AS"
+},
+{
+    years: [2017],
+    make: "Chevrolet",
+    model: "Camaro SS 1LE (V8)",
+    class: "AS"
+},
+{
+    years: [2014, 2015],
+    make: "Chevrolet",
+    model: "Camaro Z28",
+    class: "AS"
+},
+{
+    years: [2012, 2013, 2014, 2015, 2017],
+    make: "Chevrolet",
+    model: "Camaro ZL1",
+    class: "AS"
+},
+{
+    years: [2014, 2015, 2016, 2017],
+    make: "Chevrolet",
+    model: "Corvette Stingray (C7)",
+    class: "AS"
+},
+{
+    years: [2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013],
+    make: "Chevrolet",
+    model: "Corvette (C6, non-ZR1)",
+    class: "AS"
+},
+{
+    years: [2001, 2002, 2003, 2004],
+    make: "Chevrolet",
+    model: "Corvette Z06 (C5)",
+    class: "AS"
+},
+{
+    years: [2008, 2009, 2010],
+    make: "Dodge",
+    model: "Viper (non-ACR)",
+    class: "AS"
+},
+{
+    years: [1996, 1997, 1998, 1999, 2000, 2001, 2002, 2003, 2004, 2005],
+    make: "Dodge",
+    model: "Viper GTS",
+    class: "AS"
+},
+{
+    years: [1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999, 2000, 2001, 2002, 2003],
+    make: "Dodge",
+    model: "Viper R/T",
+    class: "AS"
+},
+{
+    years: [2003, 2004, 2005, 2006, 2007],
+    make: "Dodge",
+    model: "Viper SRT-10",
+    class: "AS"
+},
+{
+    years: [2012, 2013],
+    make: "Ford",
+    model: "Mustang Boss 302 Laguna Seca",
+    class: "AS"
+},
+{
+    years: [2016, 2017],
+    make: "Ford",
+    model: "Mustang Shelby GT350",
+    class: "AS"
+},
+{
+    years: [2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014],
+    make: "Ford",
+    model: "Mustang Shelby GT500",
+    class: "AS"
+},
+{
+    years: [],
+    make: "Honda",
+    model: "S2000 CR",
+    class: "AS"
+},
+{
+    years: [2014, 2015, 2016, 2017],
+    make: "Jaguar",
+    model: "F-Type (except Project 7)",
+    class: "AS"
+},
+{
+    years: [1996, 1997, 1998, 1999, 2000, 2001, 2002, 2003, 2004],
+    make: "Lotus",
+    model: "Espirit Turbo",
+    class: "AS"
+},
+{
+    years: [2010, 2011, 2012, 2013, 2014, 2015],
+    make: "Lotus",
+    model: "Evora (non-supercharged)",
+    class: "AS"
+},
+{
+    years: [1993, 1994, 1995],
+    make: "Mazda",
+    model: "RX-7",
+    class: "AS"
+},
+{
+    years: [2012, 2013, 2014, 2015, 2016],
+    make: "Mercedes-Benz",
+    model: "SLK55 AMG",
+    class: "AS"
+},
+{
+    years: [],
+    make: "Morgan",
+    model: "Plus 8",
+    class: "AS"
+},
+{
+    years: [],
+    make: "Morgan",
+    model: "Roadster V6",
+    class: "AS"
+},
+{
+    years: [2004, 2005, 2006, 2007, 2008],
+    make: "Nissan",
+    model: "350Z NISMO",
+    class: "AS"
+},
+{
+    years: [2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017],
+    make: "Nissan",
+    model: "370Z NISMO",
+    class: "AS"
+},
+{
+    years: [2007, 2008, 2009],
+    make: "Pontiac",
+    model: "Solstice GXP (Turbo)",
+    class: "AS"
+},
+{
+    years: [2017],
+    make: "Porsche",
+    model: "718 Boxter (non-S)",
+    class: "AS"
+},
+{
+    years: [2017],
+    make: "Porsche",
+    model: "718 Cayman (non-S)",
+    class: "AS"
+},
+{
+    years: [1998, 1999, 2000, 2001, 2002, 2003, 2004, 2005],
+    make: "Porsche",
+    model: "911 (996, non-turbo)",
+    class: "AS"
+},
+{
+    years: [2013, 2014, 2015, 2016],
+    make: "Porsche",
+    model: "Boxster (non-GTS, non-S, non-Spyder)",
+    class: "AS"
+},
+{
+    years: [2009, 2010, 2011,  2012],
+    make: "Porsche",
+    model: "Boxster S (987 chassis)",
+    class: "AS"
+},
+{
+    years: [2013, 2014, 2015, 2016],
+    make: "Porsche",
+    model: "Cayman (non-GT4, non-GTS, non-R, non-S)",
+    class: "AS"
+},
+{
+    years: [2009, 2010, 2011,  2012],
+    make: "Porsche",
+    model: "Cayman S (987 chassis)",
+    class: "AS"
+},
+{
+    years: [2007, 2008, 2009, 2010],
+    make: "Saturn",
+    model: "Sky Redline (Turbo)",
+    class: "AS"
+},
+{
+    years: [],
+    make: "Acura",
+    model: "NSX (non-Zanardi Edition)",
+    class: "BS"
+},
+{
+    years: [2007, 2008],
+    make: "Audi",
+    model: "RS 4",
+    class: "BS"
+},
+{
+    years: [2013, 2014, 2015],
+    make: "Audi",
+    model: "RS 5",
+    class: "BS"
+},
+{
+    years: [2003, 2004],
+    make: "Audi",
+    model: "RS 6 (C5 chassis)",
+    class: "BS"
+},
+{
+    years: [2015, 2016, 2017],
+    make: "Audi",
+    model: "S3 (2.0T)",
+    class: "BS"
+},
+{
+    years: [2010, 2011, 2012, 2013, 2014, 2015, 2016],
+    make: "Audi",
+    model: "S4",
+    class: "BS"
+},
+{
+    years: [2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017],
+    make: "Audi",
+    model: "S5",
+    class: "BS"
+},
+{
+    years: [2013, 2014, 2015, 2016, 2017],
+    make: "Audi",
+    model: "S6",
+    class: "BS"
+},
+{
+    years: [2012, 2013, 2014, 2015, 2016, 2017],
+    make: "Audi",
+    model: "S7",
+    class: "BS"
+},
+{
+    years: [2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017],
+    make: "Audi",
+    model: "TT quattro (AWD)",
+    class: "BS"
+},
+{
+    years: [2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017],
+    make: "Audi",
+    model: "TTS",
+    class: "BS"
+},
+{
+    years: [2011, 2012],
+    make: "BMW",
+    model: "1 Series M Coupe",
+    class: "BS"
+},
+{
+    years: [2001, 2002],
+    make: "BMW",
+    model: "M Coupe",
+    class: "BS"
+},
+{
+    years: [2001, 2002],
+    make: "BMW",
+    model: "M Roadster",
+    class: "BS"
+},
+{
+    years: [2016, 2017],
+    make: "BMW",
+    model: "M2 (except Performance Edition ZL9)",
+    class: "BS"
+},
+{
+    years: [2006, 2007, 2008],
+    make: "BMW",
+    model: "Z4 Coupe (incl. M)",
+    class: "BS"
+},
+{
+    years: [2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017],
+    make: "BMW",
+    model: "Z4 Roadster (incl. M)",
+    class: "BS"
+},
+{
+    years: [2016, 2017],
+    make: "Cadillac",
+    model: "ATS-V",
+    class: "BS"
+},
+{
+    years: [2017],
+    make: "Chevrolet",
+    model: "Camaro V6 1LE",
+    class: "BS"
+},
+{
+    years: [1984, 1985, 1986, 1987, 1988, 1989, 1990, 1991, 1992, 1993, 1994, 1995, 1996],
+    make: "Chevrolet",
+    model: "Corvette (C4, all)",
+    class: "BS"
+},
+{
+    years: [1997, 1998, 1999, 2000, 2001, 2002, 2003, 2004],
+    make: "Chevrolet",
+    model: "Corvette (C5, non-Z06)",
+    class: "BS"
+},
+{
+    years: [],
+    make: "DeTomaso",
+    model: "Pantera",
+    class: "BS"
+},
+{
+    years: [],
+    make: "DeTomaso",
+    model: "Mangusta",
+    class: "BS"
+},
+{
+    years: [2016, 2017],
+    make: "Ford",
+    model: "Focus RS",
+    class: "BS"
+},
+{
+    years: [],
+    make: "Honda",
+    model: "S2000 (non-CR)",
+    class: "BS"
+},
+{
+    years: [],
+    make: "Jaguar",
+    model: "XKR Coupe",
+    class: "BS"
+},
+{
+    years: [2015, 2016, 2017],
+    make: "Lexus",
+    model: "RC-F",
+    class: "BS"
+},
+{
+    years: [2002, 2003, 2004, 2005, 2006, 2007],
+    make: "Maserati",
+    model: "Coupe",
+    class: "BS"
+},
+{
+    years: [2004, 2005, 2006, 2007],
+    make: "Maserati",
+    model: "Gransport",
+    class: "BS"
+},
+{
+    years: [2002, 2003, 2004, 2005, 2006, 2007],
+    make: "Maserati",
+    model: "Spyder",
+    class: "BS"
+},
+{
+    years: [2003],
+    make: "Mazda",
+    model: "Miata Club Sport",
+    class: "BS"
+},
+{
+    years: [2007],
+    make: "Mazda",
+    model: "MX-5 Miata MS-R",
+    class: "BS"
+},
+{
+    years: [2002, 2003, 2004],
+    make: "Mercedes-Benz",
+    model: "C32 AMG",
+    class: "BS"
+},
+{
+    years: [2014, 2015],
+    make: "Mercedes-Benz",
+    model: "CLA45 AMG",
+    class: "BS"
+},
+{
+    years: [2001, 2002, 2003, 2004, 2005, 2006],
+    make: "Mercedes-Benz",
+    model: "CLK55 AMG",
+    class: "BS"
+},
+{
+    years: [2010, 2011, 2012, 2013, 2014, 2015, 2016],
+    make: "Mercedes-Benz",
+    model: "E63 AMG",
+    class: "BS"
+},
+{
+    years: [2015, 2016, 2017],
+    make: "Mercedes-Benz",
+    model: "GLA45 AMG",
+    class: "BS"
+},
+{
+    years: [2002, 2003, 2004],
+    make: "Mercedes-Benz",
+    model: "SLK32 AMG",
+    class: "BS"
+},
+{
+    years: [2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016],
+    make: "Mercedes-Benz",
+    model: "SLK350 AMG",
+    class: "BS"
+},
+{
+    years: [2005, 2006, 2007, 2008, 2009, 2010],
+    make: "Mercedes-Benz",
+    model: "SLK55 AMG",
+    class: "BS"
+},
+{
+    years: [2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015],
+    make: "Mitsubishi",
+    model: "Lancer Evolution",
+    class: "BS"
+},
+{
+    years: [2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017],
+    make: "Nissan",
+    model: "370Z (non-NISMO)",
+    class: "BS"
+},
+{
+    years: [2007, 2008, 2009, 2010],
+    make: "Pontiac",
+    model: "Solstice",
+    class: "BS"
+},
+{
+    years: [1995, 1996, 1997, 1998],
+    make: "Porsche",
+    model: "911 (993, non-turbo)",
+    class: "BS"
+},
+{
+    years: [],
+    make: "Porsche",
+    model: "968",
+    class: "BS"
+},
+{
+    years: [2009, 2010, 2011, 2012],
+    make: "Porsche",
+    model: "Boxster (non-S)",
+    class: "BS"
+},
+{
+    years: [2005, 2006, 2007, 2008],
+    make: "Porsche",
+    model: "Boxster (987, non-S)",
+    class: "BS"
+},
+{
+    years: [2005, 2006, 2007, 2008],
+    make: "Porsche",
+    model: "Boxster S",
+    class: "BS"
+},
+{
+    years: [2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012],
+    make: "Porsche",
+    model: "Cayman (non-S)",
+    class: "BS"
+},
+{
+    years: [2006, 2007, 2008],
+    make: "Porsche",
+    model: "Cayman S",
+    class: "BS"
+},
+{
+    years: [2015, 2016, 2017],
+    make: "Porsche",
+    model: "Macan S & Turbo",
+    class: "BS"
+},
+{
+    years: [],
+    make: "Saleen",
+    model: "Mustang (non-supercharged)",
+    class: "BS"
+},
+{
+    years: [2007, 2008, 2009, 2010],
+    make: "Saturn",
+    model: "Sky (non-turbo)",
+    class: "BS"
+},
+{
+    years: [],
+    make: "Shelby",
+    model: "Cobra",
+    class: "BS"
+},
+{
+    years: [2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017],
+    make: "Subaru",
+    model: "WRX STI (incl. Special Edition)",
+    class: "BS"
+},
+{
+    years: [1993.5, 1994, 1995, 1996, 1997, 1998],
+    make: "Toyota",
+    model: "Supra Turbo",
+    class: "BS"
+},
+{
+    years: [],
+    make: "TVR",
+    model: "8-cyl",
+    class: "BS"
+},
+{
+    years: [],
+    make: "TVR",
+    model: "V6",
+    class: "BS"
+},
+{
+    years: [],
+    make: "TVR",
+    model: "V8",
+    class: "BS"
+},
+{
+    years: [],
+    make: "TVR",
+    model: "V12",
+    class: "BS"
+},
+{
+    years: [2015, 2016, 2017],
+    make: "Volkswagen",
+    model: "Golf R",
+    class: "BS"
+}];
